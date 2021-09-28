04438ada1d08ed4eab43ae37f6f736ede7fff224
andro <Androiddeveloperik22git>
2021-09-28 21:15:50 +0300

    Broadcast receiver

---
+++ app/src/main/java/com/example/broadcastreceiverplayground/AirplaneModeChangedReceiver.kt
@@ -0,0 +1,16 @@
+package com.example.broadcastreceiverplayground
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AirplaneModeChangedReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val isAirplaneModeEnabled = intent?.getBooleanExtra("state", false) ?: return
+        if (isAirplaneModeEnabled) {
+            Toast.makeText(context, "Режим полета включен", Toast.LENGTH_LONG).show()
+        } else Toast.makeText(context, "Режим полета выключен", Toast.LENGTH_LONG).show()
+    }
+}
